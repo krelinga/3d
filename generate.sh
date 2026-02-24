dfb2903aebb2a9a199841b3f3ec744154fb7d80a
Andy Kreling <krelinga@gmail.com>
2026-02-24 17:18:17 +0000

    add two inche heavy base.

--- generate.sh
+++ generate.sh
@@ -8,5 +8,6 @@ mkdir -p $OUT_BASE/stl/bases
 openscad -o $OUT_BASE/stl/bases/one_inch.stl scad/bases/one_inch.scad
 openscad -o $OUT_BASE/stl/bases/four_fifths_inch.stl scad/bases/four_fifths_inch.scad
 openscad -o $OUT_BASE/stl/bases/one_inch_heavy.stl scad/bases/one_inch_heavy.scad
+openscad -o $OUT_BASE/stl/bases/two_inch_heavy.stl scad/bases/two_inch_heavy.scad
 
 echo "Generated STL files in $OUT_BASE/stl/bases"
---
+++ scad/bases/two_inch_heavy.scad
@@ -0,0 +1,7 @@
+inch = 25.4;
+$fn = 100;
+
+difference() {
+    cylinder(h = inch * 1/8, r = inch * 1);
+    cylinder(h = inch * 15/256, r = 1.1 * inch * 3/16);
+}
