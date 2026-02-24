b28e2bf73e563862bcb2c0eba2f44535c5dd3cd1
Andy Kreling <krelinga@gmail.com>
2026-02-24 04:54:33 +0000

    add one_inch_heavy.scad.

--- generate.sh
+++ generate.sh
@@ -7,3 +7,6 @@ OUT_BASE="/nas/dev/3d"
 mkdir -p $OUT_BASE/stl/bases
 openscad -o $OUT_BASE/stl/bases/one_inch.stl scad/bases/one_inch.scad
 openscad -o $OUT_BASE/stl/bases/four_fifths_inch.stl scad/bases/four_fifths_inch.scad
+openscad -o $OUT_BASE/stl/bases/one_inch_heavy.stl scad/bases/one_inch_heavy.scad
+
+echo "Generated STL files in $OUT_BASE/stl/bases"
---
+++ scad/bases/one_inch_heavy.scad
@@ -0,0 +1,7 @@
+inch = 25.4;
+$fn = 100;
+
+difference() {
+    cylinder(h = inch * 1/8, r = inch * 1/2);
+    cylinder(h = inch * 3/32, r = 1.1 * inch * 3/16);
+}
